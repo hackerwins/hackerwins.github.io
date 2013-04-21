d0103779621e41270f09e326b4a8221e403a8b69
Youngteac Hong <susukang98@gmail.com>
2013-04-21 22:40:49 +0900

    split code.html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var blog = angular.module('blog', ['ui']);
 blog.config(['$routeProvider', function($routeProvider) {
   //setup routes
-  $routeProvider.when('/', {templateUrl: '/html/article.html'});
+  $routeProvider.when('/', {templateUrl: '/html/idea.html'});
+  $routeProvider.when('/code', {templateUrl: '/html/code.html'});
   $routeProvider.when('/profile', {templateUrl: '/html/profile.html'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
